5ee133c3b7223c55cc0dc354630e205abb90ee33
Shelkopryad <welkopr9d@gmail.com>
2018-11-30 10:32:00 +0300

    OOOOH IT WORKS!!!!

--- tmp.js
+++ tmp.js
@@ -1,22 +1,47 @@
 var file;
 var oFile;
-var rABS = true;
+var workbook;
 
 function handleFileSelect(evt) {
-    file = evt.target.files[0];
-    console.log(file);
+    var files = evt.target.files;
+    var rABS = typeof FileReader !== "undefined" && (FileReader.prototype || {}).readAsBinaryString;
+    var f = files[0];
     var reader = new FileReader();
     reader.onload = function (evt) {
+        if (typeof console !== 'undefined') console.log("onload", new Date(), rABS);
         var data = evt.target.result;
         if (!rABS) data = new Uint8Array(data);
-        var workbook = XLSX.read(data, { type: rABS ? 'binary' : 'array' });
-        var worksheet = workbook.Sheets[workbook.SheetNames[0]];
-        XLSX.utils.sheet_add_aoa(worksheet, [
-            ["new data", 1, 2, 3]
-        ], { origin: -1});
-        /* DO SOMETHING WITH workbook HERE */
+        workbook = XLSX.read(data, { type: rABS ? 'binary' : 'array' });
     };
-    if (rABS) reader.readAsBinaryString(file); else reader.readAsArrayBuffer(file);
+    if (rABS) reader.readAsBinaryString(f);
+    else reader.readAsArrayBuffer(f);
 }
 
 document.getElementById('file_chooser').addEventListener('change', handleFileSelect, false);
+
+var save_btn = document.getElementById('save_btn');
+
+save_btn.onclick = function () {
+    console.log(workbook.SheetNames[0]);
+    console.log(workbook.SheetNames[1]);
+    console.log(workbook.SheetNames[2]);
+
+    var worksheet = workbook.Sheets["Sheet1"];
+    console.log(worksheet);
+    var desired_cell = worksheet['A1'];
+    console.log(desired_cell.v);
+
+    // var ws_data = [["", "Hello", "", "", "World", "", "", "", "All"]];
+
+    // XLSX.utils.sheet_add_aoa(worksheet, ws_data, { origin: -1 });
+
+    // // var ws = XLSX.utils.aoa_to_sheet(ws_data);;
+
+    // console.log(worksheet);
+
+
+
+    // // XLSX.utils.book_append_sheet(workbook, ws, "Sheet1");
+
+    // console.log(workbook);
+}
